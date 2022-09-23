e2f915b5aab327f6a8f83cf5631fb0e52cacceca
Verdster98 <nicverdi85@gmail.com>
2022-09-23 11:45:25 -0700

    variables added

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var todayBody = document.querySelector("#todayBody");
+var todaysDate = moment().format("M/D/YYYY")
+var add1Day = moment().add(1, 'days').format("M/D/YYYY");
+var add2Day = moment().add(2, 'days').format("M/D/YYYY");
+var add3Day = moment().add(3, 'days').format("M/D/YYYY");
+var add4Day = moment().add(4, 'days').format("M/D/YYYY");
+var add5Day = moment().add(5, 'days').format("M/D/YYYY");
+
+var day1 = document.querySelector("#day1")
+var day2 = document.querySelector("#day2")
+var day3 = document.querySelector("#day3")
+var day4 = document.querySelector("#day4")
+var day5 = document.querySelector("#day5")
+
